e4a6df653a741a57b93a7b67a73e39b97055dc1a
benard <benardboiwo703@gmail.com>
2024-06-15 13:21:58 +0300

    initial commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+# lib/email_address_parser.py
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        # Regular expression to find email addresses
+        pattern = r'[\w\.-]+@[\w\.-]+'
+        emails = re.findall(pattern, self.email_string)
+        return emails
+
